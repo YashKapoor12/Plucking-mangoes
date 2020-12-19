e0a742b3a61c09452cfd1b891540209e2c857dd6
YashKapoor12 <71967921+YashKapoor12@users.noreply.github.com>
2020-12-19 15:57:19 +0530

    Add files via upload

--- ground.js
+++ ground.js
@@ -12,12 +12,6 @@ class Ground{
         this.body=Bodies.rectangle(x,y,width,10,options);
         World.add(world,this.body);
     }
-    display(){
-        var pos=this.body.position;
-        push();
-        translate(pos.x,pos.y);
-        rectMode(CENTER);
-        rect( 0, 0, this.width, this.height);
-        pop()
+    display(){    
     }
 }
